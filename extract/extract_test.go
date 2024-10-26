8b851b38352fb5d0ed6b0f07fe516f32a61cb029
Cake <159018195+cakemiau@users.noreply.github.com>
2024-10-26 20:18:45 +0200

    Clean tests

--- extract/extract_test.go
+++ extract/extract_test.go
@@ -1,57 +1,112 @@
 package extract
 
 import (
+	"math"
 	"os"
 	"testing"
 )
 
-func ExtractTest(t *testing.T, url string) {
-
+// no bytes are copied to memory, the zip archive is always written to disk.
+func TestZipOnlyDisk(t *testing.T) {
 	SetVerbose(true)
 
-	dir, err := os.MkdirTemp("", "go-extract-test-*")
+	dir, err := os.MkdirTemp("", "go-extract-test-")
 	if err != nil {
 		t.Fatal(err)
 	}
-	//defer os.RemoveAll(dir)
+	defer os.RemoveAll(dir)
 
-	err = Url(url, dir, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = Url(url, dir, 1)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = Url(url, dir, 2000)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = Url(url, dir, 100000000)
-	if err != nil {
+	if err := Url("https://api.adoptopenjdk.net/v3/binary/latest/17/ga/windows/x64/jre/hotspot/normal/adoptopenjdk", dir, 0); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestZipOnlyDisk(t *testing.T) {
-	ExtractTest(t, "https://api.adoptopenjdk.net/v3/binary/latest/17/ga/windows/x64/jre/hotspot/normal/adoptopenjdk")
-}
-
+// the zip archive is initially copied to memory, but it does not fit and is moved to disk.
 func TestZipTooBig(t *testing.T) {
+	SetVerbose(true)
 
+	dir, err := os.MkdirTemp("", "go-extract-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	if err := Url("https://api.adoptopenjdk.net/v3/binary/latest/17/ga/windows/x64/jre/hotspot/normal/adoptopenjdk", dir, 5000); err != nil {
+		t.Fatal(err)
+	}
 }
 
+// the zip archive is always fully copied into memory.
 func TestZipAlwaysMem(t *testing.T) {
+	SetVerbose(true)
 
+	dir, err := os.MkdirTemp("", "go-extract-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	if err := Url("https://api.adoptopenjdk.net/v3/binary/latest/17/ga/windows/x64/jre/hotspot/normal/adoptopenjdk", dir, math.MaxInt32); err != nil {
+		t.Fatal(err)
+	}
 }
 
-func TestGzip(t *testing.T) {
-	ExtractTest(t, "https://api.adoptopenjdk.net/v3/binary/latest/17/ga/linux/x64/jre/hotspot/normal/adoptopenjdk")
+// no bytes are copied to memory, the tar archive is always written to disk.
+func TestGZipOnlyDisk(t *testing.T) {
+	SetVerbose(true)
+
+	dir, err := os.MkdirTemp("", "go-extract-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	if err := Url("https://api.adoptopenjdk.net/v3/binary/latest/17/ga/linux/x64/jre/hotspot/normal/adoptopenjdk", dir, 0); err != nil {
+		t.Fatal(err)
+	}
 }
 
-func TestZip2(t *testing.T) {
-	ExtractTest(t, "https://storage.googleapis.com/chrome-for-testing-public/130.0.6723.58/win64/chrome-win64.zip")
+// the tar archive is initially copied to memory, but it does not fit and is moved to disk.
+func TestGZipTooBig(t *testing.T) {
+	SetVerbose(true)
+
+	dir, err := os.MkdirTemp("", "go-extract-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	if err := Url("https://api.adoptopenjdk.net/v3/binary/latest/17/ga/linux/x64/jre/hotspot/normal/adoptopenjdk", dir, 5000); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// the tar archive is always fully copied into memory.
+func TestGZipAlwaysMem(t *testing.T) {
+	SetVerbose(true)
+
+	dir, err := os.MkdirTemp("", "go-extract-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	if err := Url("https://api.adoptopenjdk.net/v3/binary/latest/17/ga/linux/x64/jre/hotspot/normal/adoptopenjdk", dir, math.MaxInt32); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// alternative test for detecting archive formats
+func TestZipAlt(t *testing.T) {
+	SetVerbose(true)
+
+	dir, err := os.MkdirTemp("", "go-extract-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	if err := Url("https://storage.googleapis.com/chrome-for-testing-public/130.0.6723.58/win64/chrome-win64.zip", dir, math.MaxInt32); err != nil {
+		t.Fatal(err)
+	}
 }
